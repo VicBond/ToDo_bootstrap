7c1a9a41e6c24bdad5b1e459c08d44b9c311c79d
Victor Bondarenko <nirasimha@gmail.com>
2022-03-24 14:07:15 -0400

    trim input spaces

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const addForm = document.querySelector('.add');
 addForm.addEventListener('submit', e => {
   
   e.preventDefault();
-  const task = addForm.add.value;
+  const task = addForm.add.value.trim();
   console.log(task);
 })
