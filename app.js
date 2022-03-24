7f871ce65988f0d1f8619805c254c2458d2a2734
Victor Bondarenko <nirasimha@gmail.com>
2022-03-24 14:11:37 -0400

    add func generate tamplate

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 //add tasks - form
 
 const addForm = document.querySelector('.add');
+
+const generateTemplate = task => {
+  const html = `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${task}</span>
+        <i class="far fa-trash-alt delete"></i>
+      </li>
+  `;
+};
+
 addForm.addEventListener('submit', e => {
   
   e.preventDefault();
   const task = addForm.add.value.trim();
-  console.log(task);
-})
+  // console.log(task);
+  
+  generateTemplate(task);
+});
